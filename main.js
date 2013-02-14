880ab50d33a17accae4d3019711eed5da51bd44a
kwestein <kirsten_westeinde@hotmail.com>
2013-02-13 23:48:10 -0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+var http = require("http"),
+    nodeStatic = require("node-static"),
+    fileServer = new nodeStatic.Server(),
+    port = process.env.PORT || 1337,
+    host = process.env.IP || "127.0.0.1";
+
+http.createServer(function (request, response) {
+    request.addListener("end", function () {
+        fileServer.serve(request, response);
+    });
+}).listen(port, host);
+
+console.log("Server listening on " + host + " at port " + port);
