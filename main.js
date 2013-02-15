b6565c3f0add114d6e7a54eb504a553bde675119
kwestein <kirsten_westeinde@hotmail.com>
2013-02-15 22:35:06 +0700

    Update main.js
    Modified to use https instead of http

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-var http = require("http"),
+var https = require("https"),
     nodeStatic = require("node-static"),
     fileServer = new nodeStatic.Server(),
     port = process.env.PORT || 1337,
     host = process.env.IP || "127.0.0.1";
 
-http.createServer(function (request, response) {
+https.createServer(function (request, response) {
     request.addListener("end", function () {
         fileServer.serve(request, response);
     });
